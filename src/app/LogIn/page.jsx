bed551be25902fe9bdbe1132b37b43e0a13e751f
MILON-2377 <153436745+MILON-2377@users.noreply.github.com>
2024-06-26 00:05:15 +0800

    make responsive login page for all device

--- src/app/LogIn/page.jsx
+++ src/app/LogIn/page.jsx
@@ -1,63 +1,76 @@
+"use client";
 import React from "react";
+import { useForm } from "react-hook-form";
 import { FaGoogle } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 
 export default function LogIn() {
+
+  const {register, handleSubmit, formState:{errors}} = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+
   return (
-    <div className="flex w-full mx-auto px-5  gap-5 justify-between ">
+    <div className="flex bg-[url('https://i.ibb.co/R7MRKMn/Gemini-Generated-Image-d4ottmd4ottmd4ot.jpg')] bg-cover lg:bg-none bg-center bg-no-repeat h-screen sm:h-full w-full mx-auto px-5  gap-5 justify-between ">
       {/* login form section */}
       <div className=" w-full md:w-[70vw] lg:w-[40vw] px-6 ">
         {/* login page logo and title section */}
         <div className="flex flex-col gap-6 items-center justify-center">
-          <div className="">here logo</div>
-          <h1 className="text-4xl font-bold text-black ">Welcome Back!</h1>
-          <p className="text-gray-300 mb-5 ">Please enter your details</p>
+          <div className="h-24 w-h-24 rounded-full">
+            <img src="https://i.ibb.co/bRT22kw/Gemini-Generated-Image-j4vzpnj4vzpnj4vz.jpg" className="w-full rounded-full h-full object-cover" />
+          </div>
+          <h1 className="text-4xl font-bold text-white lg:text-black ">Welcome Back!</h1>
+          <p className="text-gray-400 text-xl mb-5 ">Please enter your details</p>
         </div>
 
         {/* social media login section */}
         <div>
           {/* login wiht google */}
           <div className="w-full grid grid-cols-3 gap-5 ">
-            <button className="items-center border border-gray-400 px-3 py-2 rounded-md justify-center flex ">
-              <FaGoogle className="text-4xl text-blue-500 " />
+            <button className="items-center border border-white bg-black bg-opacity-50 lg:opacity-0 lg:border-gray-400 px-3 py-2 rounded-md justify-center flex ">
+              <FaGoogle className="text-4xl  text-blue-500 " />
             </button>
-            <button className="items-center border border-gray-400 px-3 py-2 rounded-md justify-center flex ">
+            <button className="items-center border border-white bg-black bg-opacity-50 lg:opacity-0 lg:border-gray-400 px-3 py-2 rounded-md justify-center flex ">
               <FaFacebook className="text-4xl text-blue-500 " />
             </button>
-            <button className="items-center border border-gray-400 px-3 py-2 rounded-md justify-center flex ">
+            <button className="items-center border border-white bg-black bg-opacity-50 lg:opacity-0 lg:border-gray-400 px-3 py-2 rounded-md justify-center flex ">
               <FaTwitter className="text-4xl text-blue-500 " />
             </button>
           </div>
 
           {/* divider */}
-          <div className="divider mt-6 mb-5 ">or</div>
+          <div className="divider  mb-5 ">or</div>
 
           {/* input fields section */}
-          <form className="">
+          <form onSubmit={handleSubmit(onSubmit)} className="">
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Email Address</span>
+                <span className="label-text text-white lg:text-gray-700 ">Email Address</span>
               </label>
               <input
                 type="email"
                 placeholder="email"
-                name="email"
-                className="input input-bordered"
+                {...register("Email",{required:true, message:"email field is required!"})}
+                className="input focus:outline-none border border-yellow-500 input-bordered"
                 required
               />
+               {errors.email && <p className="text-red-500">{errors.email.message}</p>}
             </div>
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Password</span>
+                <span className="label-text text-white lg:text-gray-700 ">Password</span>
               </label>
               <input
                 type="password"
-                name="password"
+                {...register("password", {required:true, message:"password field is required"})}
                 placeholder="Password"
-                className="input input-bordered"
+                className="input focus:outline-none border border-yellow-500 input-bordered"
                 required
               />
+               {errors.password && <p className="text-red-500">{errors.password.message}</p>}
             </div>
             <div className="flex items-center mt-5 justify-between ">
               <div
@@ -65,9 +78,9 @@ export default function LogIn() {
              gap-1"
               >
                 <input type="checkbox" />
-                <span className="text-black font-semibold  ">Remember me</span>
+                <span className="lg:text-black text-white font-semibold  ">Remember me</span>
               </div>
-              <p className="text-black underline font-semibold ">
+              <p className="lg:text-black text-white underline font-semibold ">
                 Forgot Password
               </p>
             </div>
@@ -82,7 +95,7 @@ export default function LogIn() {
       {/* login images section */}
       <div className=" lg:block hidden md:flex items-center justify-center ">
         <img
-          src="https://i.ibb.co/rsd6nSt/Gemini-Generated-Image-rb0caprb0caprb0c.jpg"
+          src="https://i.ibb.co/R7MRKMn/Gemini-Generated-Image-d4ottmd4ottmd4ot.jpg"
           className="lg:w-[50vw] md:w-[50vw] md:mt-[130px] md:h-[50vh] lg:mt-0 lg:h-full object-cover "
         />
       </div>
